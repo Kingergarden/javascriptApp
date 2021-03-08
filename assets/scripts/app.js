8bae8ca6d2ffe425d3ef9d39aa3b619141a4dadd
Vehid <vehid.dziho@gmail.com>
2021-03-08 11:26:46 +0100

    working calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,180 @@
-const backdropElement = document.getElementById('backdrop');
-const modalLinkElements = document.querySelectorAll('.info-modal');
-let infoModal;
+class Calculator{
 
-function toggleBackdrop() {
-  backdropElement.classList.toggle('visible');
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+
+    clear(){
+
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
+
+
+    }
+
+    delete(){
+
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return 
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation){
+        if (this.currentOperand === '') return;
+        if (this.previousOperand != ''){
+
+            this.compute()
+
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+
+    }
+
+    compute(){
+        let computation; 
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return;
+        
+        switch(this.operation){
+                case '+' : 
+                    computation = prev + current
+                break;
+                case '*' : 
+                computation = prev * current
+                break;
+                case '-' : 
+                computation = prev - current
+                break;
+                case '÷' : 
+                computation = prev / current
+                break;
+                
+            default:
+                return;
+        }
+
+        this.currentOperand = computation
+        this.operation = undefined;
+        this.previousOperand = ''
+
+    }
+
+
+    getDisplayNumber(number)
+    {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay  
+        if (isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits:0
+            })
+        }
+            if (decimalDigits != null){
+
+                return `${integerDisplay}.${decimalDigits}` 
+            }    
+            
+            else {
+
+                return integerDisplay        
+            }
+        }
+        
+
+    updateDisplay(){
+
+        this.currentOperandTextElement.innerText = this.currentOperand;
+
+            if (this.operation != null){
+
+            this.previousOperandTextElement.innerText = 
+            `${this.previousOperand} ${this.operation}`
+            
+            } else {
+
+                this.previousOperandTextElement.innerText = '';
+            }
+
+
+        this.previousOperandTextElement.innerText = this.previousOperand;
+
+
+
+    }
+
 }
 
-function presentInfoModal(event) {
-  const text = event.target.dataset.text;
-  toggleBackdrop();
-  infoModal = document.createElement('div');
-  infoModal.classList.add('modal');
-  infoModal.innerHTML = `
-    <h2>More Details</h2>
-    <p>${text}</p>
-  `;
-  const closeButton = document.createElement('button');
-  closeButton.addEventListener('click', hideInfoModal);
-  closeButton.textContent = 'Okay';
-  infoModal.appendChild(closeButton);
-  document.body.appendChild(infoModal);
-}
 
-function hideInfoModal() {
-  toggleBackdrop();
-  document.body.removeChild(infoModal);
-}
 
-for (const linkElement of modalLinkElements) {
-  linkElement.addEventListener('click', presentInfoModal);
-}
 
-backdropElement.addEventListener('click', hideInfoModal);
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClear = document.querySelector('[data-allclear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+            calculator.appendNumber(button.innerText);
+            calculator.updateDisplay();
+    })
+
+
+})
+
+
+operationButtons.forEach(button => { 
+    button.addEventListener('click', () => {
+
+            calculator.chooseOperation(button.innerText);
+            calculator.updateDisplay();
+
+    })
+
+})
+
+equalsButton.addEventListener('click', button =>{
+
+    calculator.compute();
+    calculator.updateDisplay();
+
+})
+
+allClear.addEventListener('click', button=>{
+
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button=> {
+
+calculator.delete();
+calculator.updateDisplay();
+
+})
